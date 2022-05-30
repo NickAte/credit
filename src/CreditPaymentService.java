71dbf704362e027eaa0bc0401eda7a40f15ce821
NickAte <nikitaatepalihin2430@gmail.com>
2022-05-30 11:53:30 +0300

    com 2

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,10 +1,10 @@
 public class CreditPaymentService {
 
-    public double calculate(int a, int b, double c) {
+    public double calculate(int sum, int month, double percent) {
 
-        double monthPercent = c / 12 / 100;
-        double coefficient = monthPercent * (Math.pow((1 + monthPercent), b)) / ((Math.pow((1 + monthPercent), b)) - 1);
-        double monthPay = a * coefficient;
+        double monthPercent = percent / 12 / 100;
+        double coefficient = monthPercent * (Math.pow((1 + monthPercent), month)) / ((Math.pow((1 + monthPercent), month)) - 1);
+        double monthPay = sum * coefficient;
         return monthPay;
     }
 }
